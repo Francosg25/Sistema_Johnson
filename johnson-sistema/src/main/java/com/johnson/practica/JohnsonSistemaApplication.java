7227318357d952de4e6d41ea9880ca713eac81cd
Franco Sanchez Gutierrez <francosanchezg25@gmail.com>
2026-02-02 14:56:24 -0600

    Repositorio nuevo

---
+++ johnson-sistema/src/main/java/com/johnson/practica/JohnsonSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.johnson.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnsonSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohnsonSistemaApplication.class, args);
+	}
+
+}
---
+++ johnson-sistema/src/test/java/com/johnson/practica/JohnsonSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnson.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnsonSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
